6a633b9513477317c529a4f93a08bd0cd8cfac7f
KuWi <97820112+KuwiNet@users.noreply.github.com>
2023-04-20 12:34:19 +0800

    Update live.sh

--- live.sh
+++ live.sh
@@ -2,10 +2,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #=================================================================#
-#   System Required: CentOS7 X86_64                               #
+#   System Required: CentOS/Debian/Ubuntu/Fedora (32bit/64bit)    #
 #   Description: FFmpeg Stream Media Server                       #
-#   Author: LALA                                    #
+#   Author: LALA          Mender：KuwiNet                         #
 #   Website: https://www.lala.im                                  #
+#            https://kuwi.net                                     #
 #=================================================================#
 
 # 颜色选择
@@ -16,14 +17,14 @@ font="\033[0m"
 
 ffmpeg_install(){
 # 安装FFMPEG
-read -p "你的机器内是否已经安装过FFmpeg4.x?安装FFmpeg才能正常推流,是否现在安装FFmpeg?(yes/no):" Choose
-if [ $Choose = "yes" ];then
+read -p "你的机器内是否已经安装过FFmpeg4.x?安装FFmpeg才能正常推流,是否现在安装FFmpeg?(yes[y]/no[n]):" Choose
+if [ $Choose = "y" ];then
 	wget --no-check-certificate https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.0.3-64bit-static.tar.xz
 	tar -xJf ffmpeg-4.0.3-64bit-static.tar.xz
 	cd ffmpeg-4.0.3-64bit-static
 	mv ffmpeg /usr/bin && mv ffprobe /usr/bin && mv qt-faststart /usr/bin && mv ffmpeg-10bit /usr/bin
 fi
-if [ $Choose = "no" ]
+if [ $Choose = "n" ]
 then
     echo -e "${yellow} 你选择不安装FFmpeg,请确定你的机器内已经自行安装过FFmpeg,否则程序无法正常工作! ${font}"
     sleep 2
@@ -47,8 +48,8 @@ fi
 read -p "输入你的视频存放目录 (格式仅支持mp4,并且要绝对路径,例如/opt/video):" folder
 
 # 判断是否需要添加水印
-read -p "是否需要为视频添加水印?水印位置默认在右上方,需要较好CPU支持(yes/no):" watermark
-if [ $watermark = "yes" ];then
+read -p "是否需要为视频添加水印?水印位置默认在右上方,需要较好CPU支持(yes[y]/no[n]):" watermark
+if [ $watermark = "y" ];then
 	read -p "输入你的水印图片存放绝对路径,例如/opt/image/watermark.jpg (格式支持jpg/png/bmp):" image
 	echo -e "${yellow} 添加水印完成,程序将开始推流. ${font}"
 	# 循环
@@ -59,7 +60,7 @@ if [ $watermark = "yes" ];then
 		ffmpeg -re -i "$video" -i "$image" -filter_complex overlay=W-w-5:5 -c:v 3000k -c:a aac -b:a 92k -strict -2 -f flv ${rtmp}
 	done
 fi
-if [ $watermark = "no" ]
+if [ $watermark = "n" ]
 then
     echo -e "${yellow} 你选择不添加水印,程序将开始推流. ${font}"
     # 循环
@@ -84,7 +85,7 @@ stream_stop(){
 	}
 
 # 开始菜单设置
-echo -e "${yellow} FFmpeg无人值守循环推流 For LALA.IM ${font}"
+echo -e "${yellow} 24*7全天候无人值守循环推流 ${font}"
 echo -e "${yellow} KuwiNet修改 适用CentOS/Debian/Ubuntu/Fedora (32bit/64bit) ${font}"
 echo -e "${red} 请确定此脚本目前是在screen窗口内运行的! ${font}"
 echo -e "${green} 1.安装FFmpeg (机器要安装FFmpeg才能正常推流) ${font}"
